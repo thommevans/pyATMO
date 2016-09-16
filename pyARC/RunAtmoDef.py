1b5c293e6beac05a5a04246319e48b7c7bbe04fa
Tom Evans <tevans@astro.ex.ac.uk>
2016-09-16 15:15:32 +0100

    more tweaks to formatting of RunAtmoDef() routine for producing the ATMO input file

--- pyARC/RunAtmoDef.py
+++ pyARC/RunAtmoDef.py
@@ -26,7 +26,7 @@ def Main( ATMO ):
     out_str += 'nband = {0}\n'.format( ATMO.nband )
     out_str += 'nband_std = {0}\n'.format( ATMO.nband_std )
     out_str += '/\n'
-    out_str += '&CHEMISTRY'
+    out_str += '&CHEMISTRY\n'
     out_str += 'chem = "{0}"\n'.format( ATMO.chem )
     out_str += 'fAin = "{0}"\n'.format( ATMO.fAin )
     out_str += 'fAeqout = "{0}"\n'.format( ATMO.fAeqout )
@@ -34,7 +34,7 @@ def Main( ATMO ):
     out_str += 'fcoeff = "{0}"\n'.format( ATMO.fcoeff )
     out_str += 'print_chem = {0}\n'.format( ATMO.print_chem )
     out_str += '/\n'
-    out_str += '&CHEM_NEQ'
+    out_str += '&CHEM_NEQ\n'
     out_str += 'mixing = {0}\n'.format( ATMO.mixing )
     out_str += 'photochem = {0}\n'.format( ATMO.photochem )
     out_str += 'nmol_neq = {0}\n'.format( ATMO.nmol_eq )
@@ -45,7 +45,7 @@ def Main( ATMO ):
     out_str += 'Nmin = {0}\n'.format( ATMO.Nmin )
     out_str += 'atol = {0}\n'.format( ATMO.atol )
     out_str += '/\n'
-    out_str += '&RADTRANS'
+    out_str += '&RADTRANS\n'
     out_str += 'nrays = {0}\n'.format( ATMO.nrays )
     out_str += 'scatter = {0}\n'.format( ATMO.scatter )
     out_str += 'irrad = {0}\n'.format( ATMO.irrad )
@@ -54,12 +54,12 @@ def Main( ATMO ):
     out_str += 'rorbit = {0}\n'.format( ATMO.rorbit )
     out_str += 'murad = {0}\n'.format( ATMO.murad )
     out_str += '/\n'
-    out_str += '&OPACITY'
+    out_str += '&OPACITY\n'
     out_str += 'nkap = {0}\n'.format( ATMO.nkap )
     out_str += 'kap_smooth = {0}\n'.format( ATMO.kap_smooth )
     out_str += 'kernel_smooth = {0}\n'.format( ATMO.kernel_smooth )
     out_str += '/\n'
-    out_str += '&SOLVER'
+    out_str += '&SOLVER\n'
     out_str += 'solve_hydro = {0}\n'.format( ATMO.solve_hydro )
     out_str += 'solve_energy = {0}\n'.format( ATMO.solve_energy )
     out_str += 'minstep = {0}\n'.format( ATMO.minstep )
@@ -68,7 +68,7 @@ def Main( ATMO ):
     out_str += 'psurf = {0}\n'.format( ATMO.psurf )
     out_str += 'print_err = {0}\n'.format( ATMO.print_err )
     out_str += '/\n'
-    out_str += '&CONVECTION'
+    out_str += '&CONVECTION\n'
     out_str += 'alpha = {0}\n'.format( ATMO.alpha )
     out_str += '/'
     ofile = open( ATMO.infile_path, 'w' )
